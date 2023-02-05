bbc886b9b80a4b854a68d7681b2e0b65aac639b0
ferutizi <fer.utizi@gmail.com>
2023-02-05 18:26:50 -0300

    enviroments variables

---
+++ src/main/java/com/crud/product_list/ProductListApplication.java
@@ -0,0 +1,13 @@
+package com.crud.product_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud/product_list/ProductListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.product_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
